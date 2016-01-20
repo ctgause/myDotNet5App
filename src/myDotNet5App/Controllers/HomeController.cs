b58a8b5d0fe1bf2190740df8d319a001e493f2ed
Clayton T. Gause <ctgause@aaanet.com>
2016-01-20 12:30:47 -0500

    update1

--- src/myDotNet5App/Controllers/HomeController.cs
+++ src/myDotNet5App/Controllers/HomeController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using myDotNet5App.DataAccess;
 using Microsoft.AspNet.Mvc;
+using Npgsql;
 
 namespace myDotNet5App.Controllers
 {
@@ -10,6 +12,24 @@ namespace myDotNet5App.Controllers
     {
         public IActionResult Index()
         {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=ec2-54-227-254-152.compute-1.amazonaws.com;Port=5432;User Id=jhhviqwsvradpv;Password=FhnEg04i2FbvYcILy60R-1bAAX;Database=de2nf2u0r9g983;");
+            //NpgsqlConnection conn = new NpgsqlConnection("SERVER=ec2-54-227-254-152.compute-1.amazonaws.com;PORT=5432;TIMEOUT=30;POOLING=True;MINPOOLSIZE=1;MAXPOOLSIZE=20;COMMANDTIMEOUT=20;APPLICATIONNAME=AG;DATABASE=de2nf2u0r9g983;HOST=ec2-54-227-254-152.compute-1.amazonaws.com;USER ID=jhhviqwsvradpv;PASSWORD=FhnEg04i2FbvYcILy60R-1bAAX;sslfactory=org.postgresql.ssl.NonValidatingFactory; ");
+            // NpgsqlConnection conn =
+            // new NpgsqlConnection("Server=ec2-54-227-254-152.compute-1.amazonaws.com;Port=5432;Database=de2nf2u0r9g983;User Id=jhhviqwsvradpv;Password = FhnEg04i2FbvYcILy60R-1bAAX; ");
+            
+            //NpgsqlConnection conn =
+            //   new NpgsqlConnection(
+            //       "jdbc: postgresql://ec2-54-221-232-104.compute-1.amazonaws.com:5432/d5l5agum3a597n?user=uc8g3qurs41tam&password=pemq9248rhpkthcnrtrihbuipl5&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
+
+
+
+            conn.Open(); 
+
+            NpgsqlCommand command = new NpgsqlCommand("select version()", conn);
+            String serverversion;
+            serverversion = (String)command.ExecuteScalar();
+            ViewData["Version"] = "PostgreSQL server version: " +  serverversion;// = serverversion;
+            // MainClass.Main();
             return View();
         }
 
---
+++ src/myDotNet5App/DataAccess/Connect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Npgsql;
+
+namespace myDotNet5App.DataAccess
+{
+    public class MainClass
+    {
+        public static void PostgresConnect()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=ec2-54-227-254-152.compute-1.amazonaws.com;Port=5432;User Id=jhhviqwsvradpv;Password=FhnEg04i2FbvYcILy60R-1bAAX;Database=de2nf2u0r9g983;SSL=True;Sslmode=require;");
+            conn.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand("select version()", conn);
+            String serverversion;
+
+            try
+            {
+                serverversion = (String)command.ExecuteScalar();
+                Console.WriteLine("PostgreSQL server version: {0}", serverversion);
+            }
+
+
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
